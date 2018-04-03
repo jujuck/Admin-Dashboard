0ef70857d8f4f8bdd94fc512845c227b0d858245
jujuck <julien.y.richard@gmail.com>
2018-04-03 15:04:11 +0200

    ajout de l'onglet Mdp perdu, oublié

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function sendConnexion() {
     myRequestConnexion.open('GET', 'html/connexion.html');
     myRequestConnexion.onreadystatechange = function () {
         if (myRequestConnexion.readyState === 4) {
-            header.style.height = "250px";
+            header.style.height = "220px";
             formulaire.style.display = "inline";
             formulaire.style.height = "200px";
             formulaire.innerHTML = myRequestConnexion.responseText;
@@ -31,5 +31,26 @@ function sendInscription() {
         }
     };
     myRequestInscription.send();
+}
+
+function sendForgotMdp() {
+    formulaire.content = "";
+    var myRequestForgotMdp = new XMLHttpRequest();
+    myRequestForgotMdp.open('GET', 'html/forgotMdp.html');
+    myRequestForgotMdp.onreadystatechange = function () {
+        if (myRequestForgotMdp.readyState === 4) {
+            header.style.height = "150px";
+            formulaire.style.display = "inline";
+            formulaire.style.height = "300px";
+            formulaire.innerHTML = myRequestForgotMdp.responseText;
+        }
+    };
+    myRequestForgotMdp.send();
+}
+
+function sendAccueil() {
+    formulaire.content = "";
+    header.style.height = "400px";
+    formulaire.style.display = "none";
 }
 
