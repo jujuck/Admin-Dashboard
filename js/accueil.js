d8be2ac9d77ea3da216cab42d40d23fa5c7c8303
jujuck <julien.y.richard@gmail.com>
2018-04-03 16:28:44 +0200

    mise en place du js pour affichage des produits: bugs de d'affichage

---
+++ js/accueil.js
@@ -0,0 +1,29 @@
+//Mise en place des espaces de descritpion des produits
+function initProduit () {
+    var myRequestProduit = new XMLHttpRequest();
+    myRequestProduit.open('GET','../data/produit.json');
+
+    myRequestProduit.onreadystatechange = function () {
+        if(myRequestProduit.readyState === 4) {
+            var produits = JSON.parse(myRequestProduit.responseText);
+            var produitListe = '<div class="container">';
+
+            for (var i = 0; i < produits.length; i++) {
+                produitListe += '<div class="col-2">';
+                produitListe += '<div class="item-produits"'
+                produitListe += '<h3>' + produits[i].name + '</h3>';
+                produitListe += '<img class="image-produit" src="' + produits[i].image + '">';
+                produitListe += '<p>' + produits[i].text + '</p>';
+                produitListe += '<button class="btn">Let s go!</button>';
+                produitListe += '</div>';
+                produitListe += '</div>';
+                
+            }
+        }
+        produitListe += '</div>';
+        document.getElementById("produit").innerHTML = produitListe;
+    };
+    myRequestProduit.send();
+}
+
+initProduit();
