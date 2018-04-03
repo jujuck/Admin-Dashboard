48f119ab50b8b17fa0f3ab5230a5313203ded6e5
jujuck <julien.y.richard@gmail.com>
2018-04-03 13:59:25 +0200

    insertion des pages connexion et inscription par js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 function sendConnexion() {
-    document.getElementById(Formulaire).innerHTML = "";
+    document.getElementById("formulaire").innerHTML = "";
     var myRequestConnexion = new XMLHttpRequest();
     myRequestConnexion.open('GET', 'html/connexion.html');
     myRequestConnexion.onreadystatechange = function () {
         if (myRequestConnexion.readyState === 4) {
-            document.getElementById(formulaire).innerHTML = myRequestConnexion.responseText;
+            document.getElementById("formulaire").innerHTML = myRequestConnexion.responseText;
         }
     };
     myRequestConnexion.send();
 }
 
-sendConnexion();
+function sendInscription() {
+    document.getElementById("formulaire").innerHTML = "";
+    var myRequestInscription = new XMLHttpRequest();
+    myRequestInscription.open('GET', 'html/inscription.html');
+    myRequestInscription.onreadystatechange = function () {
+        if (myRequestInscription.readyState === 4) {
+            document.getElementById("formulaire").innerHTML = myRequestInscription.responseText;
+        }
+    };
+    myRequestInscription.send();
+}
+
