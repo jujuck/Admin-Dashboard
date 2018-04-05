74c0edbfcfb2df5c9be224b879f5d8047c8770cf
jujuck <julien.y.richard@gmail.com>
2018-04-05 16:24:12 +0200

    ajout de plusieurs graphe et mise en page

--- js/dash.js
+++ js/dash.js
@@ -37,7 +37,7 @@ var ctxPie = document.getElementById("myPie");
 var myPieChart = new Chart(ctxPie,{
     type: 'pie',
     data : {
-        labels: ['Html débutant','Html avancée','Css débutant','Bootstrap',],
+        labels: ['Html débutant','Html avancée','Css débutant','Bootstrap'],
         datasets: [{
             data: [40, 25, 20, 15],
             backgroundColor: [
@@ -53,9 +53,85 @@ var myPieChart = new Chart(ctxPie,{
     }
 });
 
+var ctxRadar = document.getElementById("myRadar");
+var myRadarChart = new Chart(ctxRadar, {
+    type: 'radar',
+    data: {
+        labels: ['Matin', 'Après-midi', 'Soir'],
+        backgroundColor: [
+                'rgba(255, 0, 54, 0.5)',
+                'rgba(235, 255, 0, 0.5)',
+                'rgba(61, 45, 253, 0.5)',
+                'rgba(23, 248, 179, 0.5)' 
+            ],
+        datasets : [{
+            label: "Html débutant",
+            data: [65, 20 , 15],
+            backgroundColor: 'rgba(255, 0, 54, 0.5)'
+        },
+        {
+            label: "Html avancé",
+            data: [35, 80 , 15],
+            backgroundColor: 'rgba(235, 255, 0, 0.5)'
+        },
+        {
+            label: "Css",
+            data: [10, 65 , 0],
+            backgroundColor: 'rgba(61, 45, 253, 0.5)'
+        },
+        {
+            label: "Bootstrap",
+            data: [25, 15 , 30],
+            backgroundColor: 'rgba(23, 248, 179, 0.5)'
+        }]    
+    }
+});
+
+var ctxPolar = document.getElementById("myPolar");
+new Chart(ctxPolar, {
+    type: 'polarArea',
+    data: {
+        labels : ["Html débutant", "Html avancé", "Css", "Bootstrap"],
+        datasets : [{
+            label: 'Lundi',
+            data: [10, 5 , 0, 0],
+            backgroundColor: 'rgba(255, 0, 54, 0.5)'
+        },
+        {
+            label: 'Mardi' ,
+            data: [0, 15 , 0, 10],
+            backgroundColor: 'rgba(235, 255, 0, 0.5)'
+        },
+        {
+            label: 'Mercredi' ,
+            data: [10, 0, 5, 5],
+            backgroundColor: 'rgba(61, 45, 253, 0.5)'
+        },
+        {
+            label:'Jeudi' ,
+            data: [0, 0 , 2, 3],
+            backgroundColor: 'rgba(23, 248, 179, 0.5)'
+        },
+        {
+            label: 'Vendredi' ,
+            data: [10, 0, 5, 5],
+            backgroundColor: 'rgba(61, 45, 253, 0.5)'
+        },
+        {
+            label: 'Samedi' ,
+            data: [10, 0, 5, 5],
+            backgroundColor: 'rgba(61, 45, 253, 0.5)'
+        },
+        {
+            label: 'Dimanche' ,
+            data: [10, 0, 5, 5],
+            backgroundColor: 'rgba(61, 45, 253, 0.5)'
+        }
+        ]
+    }
+});
+
 var menu = document.getElementById("menu-responsive");
-
-
 document.getElementById("img-menu").addEventListener("mouseover", function (e) {
     menu.style.display = "inline";
     setTimeout(function() {
